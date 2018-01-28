c9003e71344827e7bcc3e18a028460aec2bcd245
azHq <bsse0923@iit.du.ac.bd>
2018-01-29 00:04:44 +0600

    my first comment

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int a,b=6;
+    int arr[a];
+
+    char ch='s';
+
+    for(int i=1;i<10;i++){
+
+        int x=b/(i-5);
+
+
+    }
+    return 0;
+}
